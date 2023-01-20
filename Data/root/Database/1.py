033232a897fc55eed14df9f9e049544cc9765374
Linwin-Cloud <2382546858@qq.com>
2023-01-20 18:41:00 +0800

    data

--- Data/root/Database/1.py
+++ Data/root/Database/1.py
@@ -1,7 +1,14 @@
 import os
 
-for i in range(10):
+a = 0
+for i in range(100):
+    a = a +1
     os.system("touch "+str(i)+".mydb")
     with open(str(i)+".mydb","a") as f:
-        for j in range(1000):
-            f.write("Name=data"+str(j*i)+"&Value=hello"+str(j*i)+"&Type=string&createTime=2023.1.16&ModificationTime=2023.1.16&note=linwincloud\n")
+        for j in range(333):
+	        a = a + 1
+	        f.write("Name=data"+str(j*i*a)+"&Value=hello"+str(j*i)+"&Type=string&createTime=2023.1.16&ModificationTime=2023.1.16&note=linwincloud\n")
+	        f.write("Name=weixin wechat"+str(j*i*a)+"&Value=tencent"+str(j*i)+"&Type=string&createTime=2023.1.16&ModificationTime=2023.1.16&note=linwincloud\n")
+	        f.write("Name=usersfdsf"+str(j*i*a)+"&Value=11451412313123"+str(j*i)+"&Type=string&createTime=2023.1.16&ModificationTime=2023.1.16&note=linwincloud\n")
+           
+            
