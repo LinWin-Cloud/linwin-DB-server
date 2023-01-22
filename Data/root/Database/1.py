58b1d70b470c57cb35b82adc84c2c4cef5c974a6
Linwin-Cloud <2382546858@qq.com>
2023-01-22 16:51:27 +0800

    数据

--- Data/root/Database/1.py
+++ Data/root/Database/1.py
@@ -1,7 +1,7 @@
 import os
 
-for i in range(2):
+for i in range(5):
     os.system("touch "+str(i)+".mydb")
-    for j in range(2):
+    for j in range(10):
         with open(str(i)+".mydb","a") as f:
             f.write("Name=data"+str(i+j)+"&Value="+str(i+j)+"&Type=string&createTime=2023.1.1&ModificationTime=2023.1.1&note=No\n")
