f7caf25c3d4492d8a658da297be8f7a5a436f9c7
人生苦短 <an282546858@outlook.com>
2023-01-24 12:42:03 +0800

    Data Base File

--- Data/root/Database/1.py
+++ Data/root/Database/1.py
@@ -1,7 +1,7 @@
 import os
 
-for i in range(1):
+for i in range(3):
     os.system("touch "+str(i)+".mydb")
-    for j in range(1000):
+    for j in range(2):
         with open(str(i)+".mydb","a") as f:
             f.write("Name=data"+str(i+j)+"&Value="+str(i+j)+"&Type=string&createTime=2023.1.1&ModificationTime=2023.1.1&note=No\n")
