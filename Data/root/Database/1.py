9bbec00681df43a1c09ee2a20b540a7b3409e088
人生苦短 <an282546858@outlook.com>
2023-01-24 16:00:41 +0800

    Data

--- Data/root/Database/1.py
+++ Data/root/Database/1.py
@@ -1,7 +1,14 @@
 import os
 
-for i in range(3):
+os.system("rm -f *.mydb")
+for i in range(10):
     os.system("touch "+str(i)+".mydb")
-    for j in range(2):
+    with open(str(i)+".mydb","a") as a:
+        a.write("""
+{
+    "Update" : "2023.1.24"
+}
+                """)
+    for j in range(10):
         with open(str(i)+".mydb","a") as f:
             f.write("Name=data"+str(i+j)+"&Value="+str(i+j)+"&Type=string&createTime=2023.1.1&ModificationTime=2023.1.1&note=No\n")
