bd7c453fc47a29e64a89496a65d8f75f91a15dcf
LinWin-Cloud <2382546858@qq.com>
2023-02-03 11:47:58 +0800

    usr

--- usr/openData-SqlDrive/SqlDrive/Python/SqlDrive/SqlDrive.py
+++ usr/openData-SqlDrive/SqlDrive/Python/SqlDrive/SqlDrive.py
@@ -15,8 +15,35 @@ class SQL_DO(object):
     def sendMysScript(self,script:str) -> bool:
         #print(str(self.Remote)+"/?Logon="+str(self.userName)+"?Passwd="+str(self.add_MD5(self.Passwd))+"?Command="+script)
         r = requests.get(str(self.Remote)+"/?Logon="+str(self.userName)+"?Passwd="+str(self.add_MD5(self.Passwd))+"?Command="+script)
+        getText = r.text
+        split = getText.split("\n")
+        message:str = ""
+        j = 0
+        for i in split:
+            j = j + 1
+            message = message + str(i) + "\n"
         
-        return True
-
-    def outValue(self):
+        j = j - 1
+        self.Message = message
+        if j == 0:
+            return False
+        else:
+            return True
+    def getMessage(self) -> str:
+        return self.Message
+    def outValue(self) -> None:
         print(self.userName+" "+self.Passwd+" "+self.Remote)
+
+    def listDatabase(self) -> list[str]:
+        if self.sendMysScript("list database") == True:
+            return self.getMessage().split("\n")
+        else:
+            return []
+    
+    def deleteDatabase(self,name:str) -> bool:
+        return self.sendMysScript("delete database "+str(name))
+    
+    def deleteData(self,name:str,database:str) -> bool:
+        return self.sendMysScript("delete data '"+str(name)+"' in "+database)
+
+    
