d3cf08cc56349276123d3bf33003eddd84a634c7
LinWin-Cloud <2382546858@qq.com>
2023-02-06 12:18:04 +0800

    log file

---
+++ usr/openData-Server/src/Linwin/LinwinVOS/Users/api/Sudo.java
@@ -0,0 +1,11 @@
+package LinwinVOS.Users.api;
+
+public class sudo {
+    public boolean createUser() {
+        /**
+         * This API sets up databases on the cloud.
+         * This API can bypass root and execute some database system-level command lines.
+         * But cannot delete, look up and create database or data.
+         */
+    }
+}
