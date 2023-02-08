3cefab603598aed5b67459560e9843b22d050d35
LinWin-Cloud <2382546858@qq.com>
2023-02-08 13:17:33 +0800

    fix the bug and add new function in mydbshell

--- usr/openData-Server/MydbShell/Shell/MydbShell.py
+++ usr/openData-Server/MydbShell/Shell/MydbShell.py
@@ -9,16 +9,18 @@ def MainShell(user:str,passwd:str,host:str,port:int):
     if command == "clear":
         os.system("clear")
         return True
+    if command == "":
+        return True
     else:
         pathNow = os.getcwd()
-        os.system("cd '"+pathNow+"/../../release/out/' && java -jar ClientShell.jar "+host+" "+port+" "+user+" "+passwd+" \""+command+"\"")
+        os.system("../../../../opt/linwinJRE/bin/java -jar ../../release/out/ClientShell.jar "+host+" "+port+" "+user+" "+passwd+" \""+command+"\"")
 
 if __name__ == "__main__":
     host = input("Logon Host: ")
     port = input("Logon Port: ")
     user = input("Logon Users: ")
     passwd = input("Logon Passwd: ")
-    print("=======================================")
+    print("=================Login to Console====================")
     while True:
         MainShell(user,passwd,host,port)
         
