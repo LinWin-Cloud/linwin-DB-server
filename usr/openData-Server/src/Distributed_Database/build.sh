38500b7e56d7d22a4eecc7e1df6704f77c1b4033
LinWin-Cloud <2382546858@qq.com>
2023-02-12 13:33:35 +0800

    src

--- usr/openData-Server/src/Distributed_Database/build.sh
+++ usr/openData-Server/src/Distributed_Database/build.sh
@@ -1,4 +1,7 @@
 find . -type f | grep .java > source.txt
 javac -d ../../out/Distributed_Database/ @source.txt
 cd ../../out/Distributed_Database/
-java MainApp
+jar -cvfm Distributed_Database.jar ../../release/MANIFEST/Distributed_Database.MF *
+mv Distributed_Database.jar ../../release/out
+cd ../../release/out
+java -jar Distributed_Database.jar
