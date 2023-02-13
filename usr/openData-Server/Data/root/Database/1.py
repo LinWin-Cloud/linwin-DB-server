0474880ab7f96abed4ce28e0a3cb0f6122529245
LinWin-Cloud <2382546858@qq.com>
2023-02-13 14:16:31 +0800

    Data

--- usr/openData-Server/Data/root/Database/1.py
+++ usr/openData-Server/Data/root/Database/1.py
@@ -10,6 +10,6 @@ for i in range(10):
     "Update" : "2023.1.24"
 }
                 """)
-    for j in range(100):
+    for j in range(10):
         with open(str(i)+".mydb","a") as f:
             f.write("Name=data"+str(i+j)+"&Value="+str(i+j)+"&Type=string&createTime=2023.1.1&ModificationTime=2023.1.1&note=No\n")
