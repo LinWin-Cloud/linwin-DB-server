5eb1ab182dc334c552a21dd8b139267f343e8a75
LinWin-Cloud <2382546858@qq.com>
2023-02-14 21:11:34 +0800

    website

---
+++ js/index.js
@@ -0,0 +1,31 @@
+function a() 
+{
+    window.open("https://github.com/LinWin-Cloud/linwin-DB-server")
+    return true;
+}
+function b() 
+{
+    window.open("https://github.com/LinWin-Cloud/openLinwin");
+}
+function c() 
+{
+    window.open("https://gitee.com/LinwinSoft/open-data-api")
+}
+function d() 
+{
+    window.open("https://gitee.com/LinwinSoft/")
+}
+function e() 
+{
+    window.open("https://github.com/LinWin-Cloud/linwin-DB-server")
+}
+function f() {
+    alert("QQ: 2382546858")
+}
+function g() 
+{
+    window.location.href = "https://gitee.com/LinwinSoft/linwin-DB-server/releases/";
+}
+function h() {
+    window.location.href = "https://gitee.com/LinwinSoft/linwin-DB-server/wiki/home";
+}
